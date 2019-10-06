5af0311e416a4146dad3ce7fc4e0db6d3ca8a8f0
Edgars Kuzmanis <edgars.kuzmanis@scandiweb.com>
2019-10-06 21:14:34 +0300

    Updates

---
+++ precache-manifest.64a7e45c93c70ee5e5334cb4326b07b3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c6b1e410fd96d0f63b7fb6e9b7ddd73",
+    "url": "/ragem/Linedrawer/index.html"
+  },
+  {
+    "revision": "b87f268635c1a0099063",
+    "url": "/ragem/Linedrawer/static/css/main.268f036f.chunk.css"
+  },
+  {
+    "revision": "bfcb5e20407eaaa1fe9c",
+    "url": "/ragem/Linedrawer/static/js/2.ace2dc75.chunk.js"
+  },
+  {
+    "revision": "b87f268635c1a0099063",
+    "url": "/ragem/Linedrawer/static/js/main.f3d0ff26.chunk.js"
+  },
+  {
+    "revision": "7359559b324893f90218",
+    "url": "/ragem/Linedrawer/static/js/runtime-main.44f015ef.js"
+  }
+]);
